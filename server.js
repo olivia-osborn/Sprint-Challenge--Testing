e86b880bb01971479d6fb8d20b228ed4292fc66b
Olivia Osborn <olioz95@gmail.com>
2019-03-08 12:01:27 -0500

    wrote all tests for git request

--- server.js
+++ server.js
@@ -8,4 +8,12 @@ server.get("/", async (req, res) => {
   res.status(200).json("it's working!");
 });
 
+server.get("/games", async (req, res) => {
+  try {
+    games = await Games.get();
+    res.status(200).json(games);
+  } catch (error) {
+    res.status(500).json({ error: "couldn't fetch your data!" });
+  }
+});
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -12,4 +12,20 @@ describe("server.js", () => {
       expect(res.body).toEqual("it's working!");
     });
   });
+
+  describe("get /games", () => {
+    it("should return 200", async () => {
+      const res = await request(server).get("/games");
+      expect(res.status).toBe(200);
+    });
+
+    it("should retrieve list of games", async () => {
+      const res = await request(server).get("/games");
+      expect(res.body).toEqual([]);
+    });
+    it("should return JSON", async () => {
+      const res = await request(server).get("/games");
+      expect(res.type).toBe("application/json");
+    });
+  });
 });
